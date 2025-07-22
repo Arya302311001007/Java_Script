11437932ace70456ae9fb26b2798ee298ea3e0f0
Arya302311001007 <162109359+Arya302311001007@users.noreply.github.com>
2025-07-22 12:21:31 +0000

    ok

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"// treat all js code as newer version
+//alert(3+3)// we are using node.js,not Browser
+
+console.log(3+3)
+//Code readibility should be high
+console.log("Arya")
+
+let name ="Arya"//string
+let age = 18//number
+let isloggedIn=false//boolean
+
+/*
+number=>2 to the power 53
+bigint
+string=>""
+boolean=>true/false
+null=>standalone value(represntaion of empty value)
+undefined=>value is not assigned
+symbol=>unique
+object
+*/
+
+
+console.table([typeof "Arya",typeof age,typeof null,typeof undefined])
+
+//type of null is object....
+//type of undefined is undefined
