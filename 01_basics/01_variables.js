7d01921d3f27831106cfbc2c81823f43a4789411
Arya302311001007 <162109359+Arya302311001007@users.noreply.github.com>
2025-07-22 11:52:54 +0000

    First change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 14453
+let accountEmail = "arya1234@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+// accountId =2 // not allowed 
+accountEmail ="abcd@abcd.com"
+accountPassword="1223442"
+accountCity="Berhampore"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
